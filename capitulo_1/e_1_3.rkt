33eec3694825df57f4b09c9a5e170aad12cd7bcd
Víctor Castañeda Balmori <victor.castaneda@alumnos.unican.es>
2025-09-20 13:52:49 +0200

    Finished exercise 1.3

--- capitulo_1/e_1_3.rkt
+++ capitulo_1/e_1_3.rkt
@@ -1,6 +1,18 @@
 #lang racket
 
-(define (f a b c) (cond (( and (> a b) (> b c)) (+ (* a 1) (* b b)))
-      (else 25)))
+(define (square x)
+  (* x x)
+  )
 
-(f 3 2 1)
+(define (sum-of-squares-of-two-largest a b c)
+  (if (>= a b)
+      (if (>= b c)
+          (+ (square a) (square b))
+      (+ (square a) (square c))
+      )
+  (if (>= a c)
+      (+ (square a) (square b))
+  (+ (square b) (square c))
+  )
+  )
+  )

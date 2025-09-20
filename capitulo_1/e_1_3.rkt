212505c879660567f7e65140afded38be15bcd6b
Víctor Castañeda Balmori <victor.castaneda@alumnos.unican.es>
2025-09-20 16:42:24 +0200

    Corrected indentation in 1.3

--- capitulo_1/e_1_3.rkt
+++ capitulo_1/e_1_3.rkt
@@ -10,9 +10,9 @@
           (+ (square a) (square b)) ; a >= b and b >= c -> (a*a) + (b*b)
       (+ (square a) (square c)) ; a >= b and c > b -> (a*a) + (c*c)
       )
-  (if (>= a c) ; if c > a, then a >= c ?
-      (+ (square a) (square b)) ; b > a and a >= c -> (a*a) + (b*b)  
-  (+ (square b) (square c)) ; b > a and c > a -> (b*b) + (c*c)
-  )
-  )
+      (if (>= a c) ; if c > a, then a >= c ?
+          (+ (square a) (square b)) ; b > a and a >= c -> (a*a) + (b*b)
+          (+ (square b) (square c)) ; b > a and c > a -> (b*b) + (c*c)
+          )
+      )
   )

672522d0d034cf18a95d6c6c3ff7405553cdb1af
Carlos García Arenal <cga416@alumnos.unican.es>
2025-09-23 20:03:01 +0200

    Exercise 31 imroved and switched to english

--- capitulo_1/e_1_31.rkt
+++ capitulo_1/e_1_31.rkt
@@ -1,5 +1,10 @@
 #lang racket
-;1. The `sum` procedure is only the simplest of a vast number of similar
+
+;Exercise 1.31
+
+;QUESTION 1
+
+;The `sum` procedure is only the simplest of a vast number of similar
 ;abstractions that can be captured as higher-order procedures. Write an analogous
 ;procedure called `product` that returns the product of the values of a
 ;function at points over a given range.  Show how to define `factorial` in
@@ -12,41 +17,37 @@
 ; 4   3 * 3 * 5 * 5 * 7 * 7 ...
 ;```
 
-;Procedimiento RECURSIVO de multiplicación en un rango de valores
-(define (product-range-rec a b)
+;ANSWER
+
+;This is the general recursive procedure for a product in a range of numbers
+(define (product-rec term a  next b)
   (if (> a b)
       1
-      (* a (product-range-rec (+ a 1) b))))
+      (* (term a) (product-rec term (next a) next b))))
 
-;Voy a definir un procedimiento que permita hacer productorios con saltos de más de uno en cada multiplicación
-;Este es el que utilizaré para el ejercicio de aproximar pi.
-(define (product-range-step a b step)
-  (if (> a b)
-      1
-      (* a (product-range-step (+ a step) b step))))
-
-;El procedimento de factorial se puede definir fácilmente de esta forma
+;Factorial can be easily defined using the procedure we have just created
 (define (factorial n)
-  (product-range-rec 1 n))
+  (product-rec (lambda(x) x) 1 (lambda(x) (+ x 1)) n))
 
-;Procedimiento para la aproximación de pi siguiendo la fórmula
+;Now using the product procedure and the formula given for the exercise, we can approximate pi like this
+;I did not use the an exp procedure because I was not told to do so but it could be used
 (define (pi-aprox n)
-  (/ (* 8 (* (product-range-step 4 (* 2 n) 2) (product-range-step 4 (* 2 n) 2)))
-     (* (product-range-step 1 (- (* 2 n) 1) 2) (product-range-step 3 (+ (* 2 n) 1) 2)))) 
+  (/ (* 8 (* (product-rec (lambda(x) x) 4 (lambda(x) (+ x 2)) (* 2 n)) (product-rec (lambda(x) x) 4 (lambda(x) (+ x 2)) (* 2 n))))
+     (* (product-rec (lambda(x) x) 1 (lambda(x) (+ x 2)) (- (* 2 n) 1)) (product-rec (lambda(x) x) 3 (lambda(x) (+ x 2)) (+ (* 2 n) 1))))) 
         
+;QUESTION 2
 
-;2. If your `product` procedure generates a recursive process, write one that
+;If your `product` procedure generates a recursive process, write one that
 ;generates an iterative process.  If it generates an iterative process, write
 ;one that generates a recursive process.
 
-;Procedimiento ITERATIVO de multiplicación en un rango de valores
-;Añado el argumento result encapsulado en iter para acumular los resultados de las
-;multiplicaciones. Ese argumento debe empezar a 1 y lo encapsulo para no tener que
-;añadir el argumento result a la llamada del producto.
-(define (product-range-iter a b)
+;ANSWER
+
+;This is the iterative procedure
+(define (product-iter term a next b)
   (define (iter x result)
     (if (> x b)
         result
-        (iter (+ x 1) (* x result))))
+        (iter (next x) (* (term x) result))))
   (iter a 1))
       

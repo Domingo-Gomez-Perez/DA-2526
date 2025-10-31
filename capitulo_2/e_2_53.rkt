cfd038d3106003ef10f4cba12c254907cfe23f36
DavideMara <davidemaracaglia@yahoo.com>
2025-10-31 16:51:35 +0100

    Correction of the first 2 comments

--- capitulo_2/e_2_53.rkt
+++ capitulo_2/e_2_53.rkt
@@ -1,7 +1,7 @@
 #lang racket
-(list 'a 'b 'c) ; the output is (a b c), beacuse it will returns a list with inside the 3 elements "a" "b" "c" 
-(list (list 'george)) ; the output is ((george)), beacuse it will returns the elements of a list with inside another list where the only element is "george"
-(cdr '((x1 x2) (y1 y2))) ; the output is ((y1 y2)), beacuse cdr returns the tail of the list
+(list 'a 'b 'c) ; the output is (a b c), because it creates a list with the elements a, b, and c
+(list (list 'george)) ; the output is ((george)), beacause it creates a list containing another list with the element george
+(cdr '((x1 x2) (y1 y2))) ; the output is ((y1 y2)), beacuse cdr returns the rest of the list after removing the first element
 (cadr '((x1 x2) (y1 y2))) ; the output is (y1 y2), because cadr returns the second element of the list
 (pair? (car '(a short list))) ; the output is #f, because car returns the first element of the list, which is 'a, and 'a is not a pair
 (memq 'red '((red shoes) (blue socks))) ; the output is #f, because memq looks for the exact element in the list, and 'red is not equal to '(red shoes)
